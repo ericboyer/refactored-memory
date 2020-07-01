b82a3fffc80305ed76a6fc2dfd22ef2149c6f495
Eric Boyer <eboyer@redhat.com>
2020-07-01 15:46:33 -0500

    WIP - build tcp client/server

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # refactored-memory
 Simple TCP server in python
+
+# Prereqs
+* Gradle 6+
+* Python 3.7+
+
+# Build w/gradle
+```gradle clean build``` or ```./gradlew clean build```
+
+# Push to nexus
+```python3 -m twine upload --repository-url <repo_url> build/distributions/*```
+
+> repo_url = http://nexus-ebo-cicd.apps.ccsd3.rht-labs.com/repository/pypi-public/
+
+# Install package
+```TBD```
+
+# Run server
+```python3 ```
---
+++ src/server/server.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+
+
+class Server:
+    def __init__(self, ip, port):
+        self.bind_ip = ip
+        self.bind_port = port
+
+    def handle_client_connection(client_socket):
+        request = client_socket.recv(1024)
+        print('Received {}'.format(request))
+        response = "hello"
+        client_socket.sendall(response.encode('utf-8'))
+        client_socket.close()
+
+    def run(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self.bind_ip, self.bind_port))
+        s.listen(5)  # max backlog of connections
+
+        print("Listening on {}:{}".format(self.bind_ip, self.bind_port))
+
+        while True:
+            client_socket, address = s.accept()
+            print("Accepted connection from {}:{}".format(address[0], address[1]))
+            t = threading.Thread(
+                target=self.handle_client_connection,
+                # without comma you'd get a... TypeError: handle_client_connection() argument after
+                # * must be a sequence, not _socketobject
+                args=(client_socket,)
+            )
+            t.start()
+
+
+if __name__ == "__main__":
+    server = Server('0.0.0.0', 8088)
+    server.run()
