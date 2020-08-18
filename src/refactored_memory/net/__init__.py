67fe05482d8af0fe91f02b633207a6f6e4df661a
Eric Boyer <eboyer@redhat.com>
2020-08-18 09:36:34 -0500

    Minor clean-up of init files

--- src/refactored_memory/net/__init__.py
+++ src/refactored_memory/net/__init__.py
@@ -1,4 +1,2 @@
 # pkgutil-style namespace packages
-# __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-# name = 'net'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
--- src/refactored_memory/web/__init__.py
+++ src/refactored_memory/web/__init__.py
@@ -1,4 +1,2 @@
 # pkgutil-style namespace packages
-# __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-# name = 'web'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
