f5da154f60fdfe9a5291b3b5f856f9d54d0f87da
Eric Boyer <eboyer@redhat.com>
2020-09-01 10:23:32 -0500

    Add rollout latest to dc

--- src/resources/scripts/cleanup-app.sh
+++ src/resources/scripts/cleanup-app.sh
@@ -3,10 +3,11 @@
 label=refactored-memory
 
 # set defaults for dev and cicd tools project
-[[ -z $DEV_PROJECT ]] && PROJECT=refactored-memory-dev
+[[ -z $DEV_PROJECT ]] && DEV_PROJECT=refactored-memory-dev
 [[ -z $CICD_PROJECT ]] && CICD_PROJECT=ebo-cicd
 
-for project in $CICD_PROJECT $DEV_PROJECT
-do
-  oc delete all -l build=${label}
-done
+oc delete bc -l build=${label} -n $CICD_PROJECT
+oc delete all -l app=refactored-memory-frontend -n $DEV_PROJECT
+oc delete all -l app=refactored-memory-backend -n $DEV_PROJECT
+oc delete rolebinding edit -n $DEV_PROJECT
+oc delete cm $label -n $DEV_PROJECT
