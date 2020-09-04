7cbad5415e49419558e3d33e270cc24984b1d8a4
Eric Boyer <eboyer@redhat.com>
2020-09-04 13:57:03 -0500

    Add tests

---
+++ tests/net/test_client.py
@@ -0,0 +1,21 @@
+import unittest
+
+from refactored_memory.net.client import Client
+
+# TODO: write meaningful tests
+class ClientTestCase(unittest.TestCase):
+    def test_init(self):
+        ip = '0.0.0.0'
+        port = 80
+        c = Client(ip, port)
+
+        self.assertIsNotNone(c)
+        self.assertEqual(ip, c.server_ip)
+        self.assertEqual(port, c.server_port)
+
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/web/test_app.py
@@ -0,0 +1,10 @@
+import unittest
+
+# TODO: write meaningful tests
+class AppTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
