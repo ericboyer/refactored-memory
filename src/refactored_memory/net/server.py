771c769b19082b852242f453f013dfba375c8c1a
Eric Boyer <eboyer@redhat.com>
2020-09-09 16:21:26 -0500

    Add image

--- src/refactored_memory/net/server.py
+++ src/refactored_memory/net/server.py
@@ -56,7 +56,7 @@ class Server:
             print('Closing connection')
             if not client_socket is None:
                 client_socket.close()
-                
+
 
 if __name__ == '__main__':
     main()
